fca7e33a0a21d7b19102ec9402af4c8a8f5b60c6
pyphil <63059088+pyphil@users.noreply.github.com>
2021-06-06 11:55:37 +0200

    Update wlan-codes.py

--- wlan-codes.py
+++ wlan-codes.py
@@ -129,7 +129,6 @@ class CodeAbruf(Ui_CodeAbrufen, QtWidgets.QDialog):
 
         # Stylesheet non-editable Combobox ändern
         self.comboBoxLaufzeit.setStyleSheet("combobox-popup: 0;")
-        self.comboBoxLaufzeit.setPlaceholderText("placeholderText")
 
         self.count = self.db.count()
 
